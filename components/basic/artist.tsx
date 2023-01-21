c159b21c72412fbdd3d20d3ad7389633336b7467
Steward van Treel <s.vtreel@me.com>
2023-01-21 10:36:40 +0100

    deployment_test_2

--- components/basic/artist.tsx
+++ components/basic/artist.tsx
@@ -13,7 +13,7 @@ export default function Component( props: Props ) {
 
         return <>
                 <p className={ s.artist } style = { style }>
-                      - { props.text }
+                      hi { props.text }
                 </p>
         </>
 }
