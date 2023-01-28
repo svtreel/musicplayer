6d9c540a38ef4b52220619772a0945d75d5247de
Steward van Treel <s.vtreel@me.com>
2023-01-28 15:32:12 +0100

    env change

--- src/pages/api/getmeta.tsx
+++ src/pages/api/getmeta.tsx
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     try {
 
-        const url = "http://192.168.0.32/s/bluesound/nodemeta.py"
+        const url = "http://0.0.0.0/s/bluesound/nodemeta.py"
         const r = await fetch( url )
         const data = await r.json().then( ( data ) => (
           res.status( 200 ).json( { data } )
