820429f9a8433fcbfb2766f6bc77922584aa3bbf
Steward van Treel <s.vtreel@me.com>
2023-02-11 11:53:12 +0100

    adjustments + display handling impr

--- src/pages/api/shutdown.tsx
+++ src/pages/api/shutdown.tsx
@@ -1,22 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+
 type Data = {
-  data: object
+    data: object
 }
-export default async function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
-  try {
 
-    const url = "http://192.168.0.31/s/bluesound/displayOff.py"
-    const r = await fetch( url )
-    const data = await r.json().then( ( data ) => (
-      res.status( 200 ).json( { data } )
-    ))
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-  } catch ( err ) {
+    try {
 
-      const data = { error: 'failed to load data' }
-      res.status( 500 ).json( { data } )
-      console.log( data )
+        const url = "http://192.168.0.32/s/bluesound/disiplayoff.py"
+        const r = await fetch( url )
+        const data = await r.json().then( ( data ) => (
+          res.status( 200 ).json( { data } )
+        ))
 
-  }
+    } catch ( err ) {
+
+        const data = { error: 'failed to load data' }
+        res.status( 500 ).json( { data } )
+        console.log( data )
+
+    }
+
 }
